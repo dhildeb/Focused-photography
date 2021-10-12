554ae1bafc06df62702927a2b2a8314611a0ca8a
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-10-11 21:59:14 -0600

    temp about jen transitions

--- Focused-photography.client/src/pages/JenPage.vue
+++ Focused-photography.client/src/pages/JenPage.vue
@@ -2,30 +2,30 @@
   <!-- jen -->
   <div class="container-fluid my-5">
     <div class="row justify-content-around">
-      <div class="click z-1" @click="state.bag=!state.bag, state.jen=false">
-        <p class="position-absolute pro-B-font t-xl mt-5" v-if="!state.bag">
+      <div id="bag-click" class="click z-1" @click="bagTransition()">
+        <p class="position-absolute pro-B-font t-xl mt-5">
           What's In My Bag?
         </p>
       </div>
-      <div class="click z-1 mr-xl" @click="state.jen=!state.jen, state.bag=false">
+      <div id="about-click" class="click z-1 mr-xl" @click="jenTransition()">
         <div>
-          <p class="position-absolute city-font t-xxxl mt-3 ml-1" v-if="!state.jen">
+          <p class="position-absolute city-font t-xxxl mt-3 ml-1">
             about
           </p>
-          <p class="position-absolute pro-B-font t-xl mt-5 ml-4" v-if="!state.jen">
+          <p class="position-absolute pro-B-font t-xl mt-5 ml-4">
             JENNIFER
           </p>
         </div>
       </div>
     </div>
     <div class="row justify-content-center bg-white">
-      <div :class="state.bag ? 'col-4' : 'col-4 d-none'">
-        <img class="img-fluid bag" src="../assets/img/bag.png" alt="camera bag">
+      <div id="bag" class="col-4 bag hide-bag">
+        <img class="img-fluid" src="../assets/img/bag.png" alt="camera bag">
       </div>
-      <div class="self-img d-flex flex-column flex-wrap align-content-between">
+      <div id="jen" class="self-img d-flex flex-column flex-wrap align-content-between">
         <img class="img-fluid jen" src="https://firebasestorage.googleapis.com/v0/b/focused-47421.appspot.com/o/static%20images%2Fself.jpg?alt=media&token=4847c646-8bb2-4ce9-a873-d8c0c5ebfeb7" alt="jen">
       </div>
-      <div :class="state.jen ? 'col-4' : 'col-4 d-none'">
+      <div id="about-jen" class="col-4 hide-about about-jen">
         <p class="position-absolute text-focus-dark city-font t-xxxl mt-3 ml-1">
           about
         </p>
@@ -48,6 +48,7 @@
 
 <script>
 import { reactive } from '@vue/reactivity'
+import $ from 'jquery'
 export default {
   name: 'Jen',
   setup() {
@@ -56,7 +57,19 @@ export default {
       bag: false
     })
     return {
-      state
+      state,
+      bagTransition() {
+        $('#bag').toggleClass('hide-bag')
+        $('#jen').toggleClass('jen-bag')
+        $('#bag-click').addClass('d-none')
+        $('#about-click').addClass('d-none')
+      },
+      jenTransition() {
+        $('#about-jen').toggleClass('hide-about')
+        $('#jen').toggleClass('about-jen-self')
+        $('#bag-click').addClass('d-none')
+        $('#about-click').addClass('d-none')
+      }
     }
   }
 }
@@ -67,16 +80,47 @@ export default {
 .bg-pink{
   height:60px;
 }
-.d-none{
-  transition: all 1s linear;
+.about-jen{
+  transition: all 1.5s linear;
+  position: absolute;
+  left: 800px;
+}
+.hide-about{
+  transition: all 1.5s linear;
+  position: absolute;
+  left: 1400px;
 }
 .jen{
   object-fit: cover;
   height: 60vh;
 }
+.self-img{
+  transition: all 1s linear;
+  position: relative;
+  left: 0px;
+  right: 0px;
+}
+.about-jen-self{
+  transition: all 1s linear;
+  position: relative;
+  right: 200px
+}
+.jen-bag{
+  transition: all 1s linear;
+  position: relative;
+  left: 200px
+}
 .bag{
+  transition: all 1.5s linear;
+  position: absolute;
   transform: scale(1.5);
   margin-top: 4rem;
+  right: 700px;
+}
+.hide-bag{
+  transition: all 1.5s linear;
+  position: absolute;
+  right: 1300px;
 }
 .z-1{
   z-index: 1;
